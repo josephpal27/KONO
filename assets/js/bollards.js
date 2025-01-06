f45b911ab1221f6302e00581746f47f92928747e
Joseph Pal <josephpal27@gmail.com>
2025-01-06 13:03:20 +0530

    Update contact page styles and enhance image paths for rounded cards

--- assets/js/bollards.js
+++ assets/js/bollards.js
@@ -16,8 +16,8 @@ document.querySelectorAll('.web-rounded-card').forEach((card, index) => {
   // Functionality For Product Page Rounded Cards Hover on MOBILE SCREEN
   document.querySelectorAll('.mobile-rounded-card').forEach((card, index) => {
     let img = card.querySelector('img');
-    let originalSrc = `assets/images/rounded-cards/rounded-card-${index + 1}.webp`;
-    let hoverSrc = `assets/images/rounded-cards/rounded-card-${index + 1}-light.webp`;
+    let originalSrc = `assets/images/rounded-cards/bollards/rounded-card-${index + 1}.jpg`;
+    let hoverSrc = `assets/images/rounded-cards/bollards/rounded-card-${index + 1}-light.jpg`;
   
     img.addEventListener('mouseenter', () => {
         img.setAttribute('src', hoverSrc);
@@ -48,8 +48,8 @@ document.querySelectorAll('.web-rounded-card').forEach((card, index) => {
   // Functionality For Product Page Cards Slider Hover on MOBILE SCREENS
   document.querySelectorAll('.mobile-slider-container .card').forEach((card, index) => {
     let img = card.querySelector('img');
-    let originalSrc = `assets/images/product-slider/product-slide-${index + 1}.webp`;
-    let hoverSrc = `assets/images/product-slider/product-slide-${index + 1}-light.webp`;
+    let originalSrc = `assets/images/product-slider/bollards/product-slide-${index + 1}.jpg`;
+    let hoverSrc = `assets/images/product-slider/bollards/product-slide-${index + 1}-light.jpg`;
   
     img.addEventListener('mouseenter', () => {
         img.setAttribute('src', hoverSrc);
